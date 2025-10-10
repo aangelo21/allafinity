4dcbdd494ddbf24ed1be5143e0d3b8b4d2a5ffbc
Angelo <martinezglielmo@gmail.com>
2025-10-10 18:22:40 +0100

    created layout view

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>allafinity</title>
+    </head>
+    <body>
+        <h1>allafinity</h1>
+
+        <div class="container">
+            @yield('name')
+        </div>
+    </body>
+</html>
