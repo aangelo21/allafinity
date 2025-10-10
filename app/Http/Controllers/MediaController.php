5123792582f5e3514065a01dbd6286113eb85128
Angelo <martinezglielmo@gmail.com>
2025-10-10 18:02:11 +0100

    created controller for media table

---
+++ app/Http/Controllers/MediaController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Media;
+
+class MediaController extends Controller
+{
+    public function index(){
+        $media = Media::all();
+        return view('media.index', compact('media'));
+    }
+
+    public function create(){
+        return view('media.create');
+    }
+
+    public function store(Request $request){
+        $media = new Media;
+        $media->title = $request->input('title');
+        $media->category = $request->input('category');
+        $media->genre = $request->input('genre');
+        $media->grade = $request->input('grade');
+        $media->save();
+
+        return redirect()->route('media.index');
+    }
+}
